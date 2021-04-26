e19019bd874b0132768c7abe56375433cf7ec0d6
Rafael Vasconcelos <rafael.vasconcelos@itenabler.com.br>
2021-04-26 11:11:56 -0300

    terminando pagina 7

--- 7-condicionais.js
+++ 7-condicionais.js
@@ -5,26 +5,26 @@ const listaDeDestinos = new Array(
     `Rio de Janeiro`
 );
 
-const idadeComprador = 15;
+const idadeComprador = 20;
 const estaAcompanhada = false;
+const temPassagemComprada = true;
 
 console.log("Destinos possíveis:");
 console.log(listaDeDestinos);
 
-if(idadeComprador>=18){
-    console.log("Comprador maior de idade");
-    listaDeDestinos.splice(1,1);
-}else{
-    if(estaAcompanhada){
-        console.log("Comprador esta acompanhado");
-        listaDeDestinos.splice(1,1);
-    }else{
-        console.log("Comprador não é maior de idade e não posso vender");
-    }
-    
+if (idadeComprador >= 18 || estaAcompanhada ==true) {
+    console.log("Boa viagem!");
+    listaDeDestinos.splice(2, 1);
+} else {
+    console.log("Comprador não é maior de idade e não posso vender");
 }
 
-
+console.log("Embarque: \n\n");
+if(idadeComprador >= 18 && temPassagemComprada == true){
+    console.log("Boa viagem");
+} else {
+    console.log("Você não pode embarcar.")
+}
 
 console.log(listaDeDestinos);
 
