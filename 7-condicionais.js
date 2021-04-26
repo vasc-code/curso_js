e31155dc1234df38b4d0f75cb22a44bc08437129
Rafael Vasconcelos <rafael.vasconcelos@itenabler.com.br>
2021-04-26 10:55:32 -0300

    novas alterações

--- 7-condicionais.js
+++ 7-condicionais.js
@@ -6,6 +6,7 @@ const listaDeDestinos = new Array(
 );
 
 const idadeComprador = 15;
+const estaAcompanhada = false;
 
 console.log("Destinos possíveis:");
 console.log(listaDeDestinos);
@@ -14,17 +15,23 @@ if(idadeComprador>=18){
     console.log("Comprador maior de idade");
     listaDeDestinos.splice(1,1);
 }else{
-    console.log("Comprador não é maior de idade e não posso vender");
+    if(estaAcompanhada){
+        console.log("Comprador esta acompanhado");
+        listaDeDestinos.splice(1,1);
+    }else{
+        console.log("Comprador não é maior de idade e não posso vender");
+    }
+    
 }
 
 
 
 console.log(listaDeDestinos);
 
-console.log(idadeComprador>18);
-console.log(idadeComprador<18);
-console.log(idadeComprador>=18);
-console.log(idadeComprador<=18);
-console.log(idadeComprador==18);
+///console.log(idadeComprador>18);
+//console.log(idadeComprador<18);
+//console.log(idadeComprador>=18);
+//console.log(idadeComprador<=18);
+//console.log(idadeComprador==18);
 
 
