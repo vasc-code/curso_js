2c235877dbd652f90b9a6da5c9ddad2d67ef7edb
Rafael Vasconcelos <rafael.vasconcelos@itenabler.com.br>
2021-04-26 10:16:02 -0300

    adicionando pagina 6

---
+++ 6-listas.js
@@ -0,0 +1,15 @@
+console.log(`Trabalhando com listas`);
+//const salvador = ;
+//const saoPaulo = ;
+//const rioDeJaneiro = ;
+
+const listaDeDestinos = new Array(
+    `Salvador`,
+    `São Paulo`,
+    `Rio de Janeiro`
+);
+
+listaDeDestinos.push(`Curitiba`);
+
+console.log("Destinos possíveis:");
+console.log(listaDeDestinos);
