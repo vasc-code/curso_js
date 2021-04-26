7b1764a1ea82cc6882f4775fcd3034bec29084bc
Rafael Vasconcelos <rafael.vasconcelos@itenabler.com.br>
2021-04-26 09:32:37 -0300

    pag 5

---
+++ 5-atribuiçãoDeVariaveis.js
@@ -0,0 +1,12 @@
+console.log("Trabalhando com atribuição de variáveis");
+
+const idade = 20;
+let nome = "Rafael";
+const sobrenome = "Vasconcelos";
+
+//console.log(nome + " " + sobrenome);
+console.log(nome,sobrenome);
+console.log(`Meu nome é ${nome} ${sobrenome}`);
+nome = nome + sobrenome;
+console.log(nome);
+
