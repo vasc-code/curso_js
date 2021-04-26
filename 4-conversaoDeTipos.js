f3d48de34692503889db23ea6bb5911956d031c9
Rafael Vasconcelos <rafael.vasconcelos@itenabler.com.br>
2021-04-26 09:21:15 -0300

    add coisas novas

--- 4-conversaoDeTipos.js
+++ 4-conversaoDeTipos.js
@@ -6,5 +6,9 @@ console.log("2" + "2");
 console.log(parseInt("2")+parseInt("2"));
 
 
-console.log("7"/"2"); //Not a Number
+console.log("7"/"2"); 
 console.log("Rafael"/"2"); //Not a Number
+
+console.log(6.5);
+console.log(6,5);
+
